41bb5ea9496dd0f9783b5ca8310a5d1abf8aaea4
Prabhjot Singh <starcoder19@gmail.com>
2020-08-09 05:08:42 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,53 @@
+# THIS IS WHY, you should learn VIM
+# do your projects twice as fast
+# freelancer, you can get paid twice as much
+
+# your productivity for the next 2 months will
+# drop down by 60%. Frustrating
+
+# growth becomes expontential
+# within 1 year you'll be able to see an ROI
+# you'll be able to write 1.75x - 2x more code
+# cut down your learning curve from
+# 3 years of time down to 1 year of time
+
+
+# TECHNICAL TRAINING PART (80/20)
+# === VIM ITSELF (80/20) ===
+# i - insert before
+# a - insert after
+# esc - vim mode
+
+# === MOVEMENT (80/20) ===
+# h - left
+# j - down
+# k - up
+# l - right
+# e - move by words (cursor at end)
+# b - move backwards by words
+# w - move by words (cursor at start)
+# ctrl+d - move page down
+# ctrl+u - move page up
+
+# === Practical shortcuts (80/20) ===
+# dd - this deletes a line
+# 2dd - delete 2 lines (10dd deletes 10 lines)
+# cc - change a line
+# D - delete from where your cursor is
+# C - change from where your cursor is
+# x - remove character
+# u - undo
+# ctrl+r - redo
+# p - paste
+# P - paste on top
+# yy - yank (copy)
+# ciw - change word
+# diw - delete word
+# yiw - yank word (copy word)
+# I - insert before line
+# A - insert after line
+
+# === Fancy Stuff ===
+# r - replace
+# ci( - change stuff inside parenthesis
+# ci[ - change stuff inside brackets
